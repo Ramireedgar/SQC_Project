f373daead334c9cff13ba17990fd6b816dbc112a
Edgar Ramirez <ramirezedgar0830@gmail.com>
2025-12-09 10:20:23 -0500

    added employee menu loop for input.

--- src/Employee.java
+++ src/Employee.java
@@ -2,6 +2,8 @@
  * user subclass for normal employee users to access their own data only
  */
 
+import java.util.Scanner;
+
 public class Employee extends User {
     public Employee(int empId, String username, String password) {
         super(empId, username, password);
@@ -9,9 +11,33 @@ public class Employee extends User {
 
     @Override
     public void showMenu() {
-        System.out.println("Employee Menu:");
-        System.out.println("1. View Personal Data");
-        System.out.println("2. Logout");
-    }
+        Scanner scanner = new Scanner(System.in);
+        int option = 0;
 
+        do {
+            System.out.println("\n=== Employee Menu ===");
+            System.out.println("1. View Personal Data");
+            System.out.println("2. Logout");
+            System.out.print("Select an option: ");
+
+            try {
+                String input = scanner.nextLine();
+                option = Integer.parseInt(input);
+
+                switch (option) {
+                    case 1:
+                        // You will need to implement a 'getById' using this.getEmpId() later
+                        System.out.println("Personal Data View: (Feature coming soon...)");
+                        break;
+                    case 2:
+                        System.out.println("Logging out...");
+                        break;
+                    default:
+                        System.out.println("Invalid option, please try again.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input! Please enter a number.");
+            }
+        } while (option != 2);
+    }
 }
